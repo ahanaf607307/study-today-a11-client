009a7c136cd7ca71efa5a45da3eab4e602625657
ahanaf607307 <ahanaf607307@gmail.com>
2024-12-24 03:38:07 +0600

    added banner in assignment page

--- src/Pages/Assignment.jsx
+++ src/Pages/Assignment.jsx
@@ -54,9 +54,27 @@ const [assignments , setAssignments] = useState([])
 
 
   return (
-    <div className='p-10 bg-cyan-50/50'>
-      <h1 className='my-10 text-3xl font-bold text-center'>Total Assignment's - {assignments.length}</h1>
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8 '>
+    <div className='p-10 '>
+      <div className='mt-6 mb-14  h-96 w-full bg-no-repeat bg-center  bg-cover bg-pendingBg bg-blend-darken rounded-2xl'>
+   <div className=' bg-black/60 w-full h-full flex flex-col justify-center items-center rounded-2xl'>
+   <h1 className='text-5xl text-center font-bold text-orange-500'>  WHY CHOOSE ONLINE LEARNING?
+   </h1>
+   <h1 className='text-xl text-center font-bold text-white/80 mt-5'> We will give you many benefits when using online learning
+   </h1>
+   </div>
+     
+    </div>
+
+  <div className='my-10'>
+  <h1 className='text-5xl text-center font-bold text-orange-500'> POPULAR ASSIGNMENT'S
+   </h1>
+   <h1 className='text-md text-center font-bold text-black/60 mt-5'> We have monthly 1000+ request to be joined assignment's exm.
+   </h1>
+  </div>
+
+
+    
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8 mb-10'>
       {
         assignments?.map(card => <AssignCard key={card?._id} card={card} handleDelete={handleDelete}/>)
       }
